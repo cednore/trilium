516d2561e766ccb345356f194b9104a5b2131c57
cednore <cednore@gmail.com>
2022-06-30 07:01:50 -0700

    ⚰️ remove unncessary vars

--- modules/trilium-app/variables.tf
+++ modules/trilium-app/variables.tf
@@ -22,11 +22,6 @@ variable "stage" {
   default     = "production"
 }
 
-variable "vpc_id" {
-  description = "ID of root VPC"
-  type        = string
-}
-
 variable "subnet_id" {
   description = "ID of subnet to place the instance"
   type        = string
@@ -47,45 +42,4 @@ variable "instance_pubkey" {
   description = "Public key for ssh connection to instances"
   type        = string
   sensitive   = true
-}
-
-variable "instance_privkey_path" {
-  description = "Path to private key file for ssh connection to instances"
-  type        = string
-}
-
-variable "app_image" {
-  description = "App docker image"
-  type        = string
-  default     = "zadam/trilium"
-}
-
-variable "app_image_tag" {
-  description = "Tag for app docker image"
-  type        = string
-  default     = "0.52.3"
-}
-
-variable "app_container_count" {
-  description = "Number of app containers to run"
-  type        = number
-  default     = 1
-}
-
-variable "app_container_name" {
-  description = "Name prefix of app containers"
-  type        = string
-  default     = "app"
-}
-
-variable "app_container_ports" {
-  description = "Ports to publish from the container to the host"
-  type        = string
-  default     = "8080:8080"
-}
-
-variable "app_container_volumes" {
-  description = "Volumes to mount within the container"
-  type        = string
-  default     = "/mnt/app_data:/home/node/trilium-data"
 }
