b1f7930d6c46fe90dfee2d5d8f7443de4ec81f58
cednore <cednore@gmail.com>
2022-06-30 06:50:04 -0700

    🔥 remove provisioner from app instance

--- modules/trilium-app/ec2.tf
+++ modules/trilium-app/ec2.tf
@@ -5,21 +5,6 @@ resource "aws_instance" "app" {
   key_name               = aws_key_pair.main.key_name
   vpc_security_group_ids = var.instance_sg_ids
 
-  provisioner "local-exec" {
-    command = <<BASH
-                sleep 10s && ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/app-playbook.yml \
-                  -u ubuntu \
-                  -i '${self.public_ip},' \
-                  --private-key ${var.instance_privkey_path} \
-                  -e 'image=${var.app_image}' \
-                  -e 'tag=${var.app_image_tag}' \
-                  -e 'container_count=${var.app_container_count}' \
-                  -e 'container_name=${var.app_container_name}' \
-                  -e 'ports=${var.app_container_ports}' \
-                  -e 'volumes=${var.app_container_volumes}'
-              BASH
-  }
-
   tags = merge(local.default_tags, {
     Name = "i-${local.slug}-app"
   })
