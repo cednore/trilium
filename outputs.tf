aebfaeda7231dd674f9950e08a166c6477061749
cednore <cednore@gmail.com>
2022-07-01 14:06:28 -0700

    🔥 remove instance public ip output

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,6 @@ output "app_urls" {
   value       = concat(["https://${var.domain}"], [for name in var.alternative_domains : "https://${name}"])
 }
 
-output "app_instance_public_ip" {
-  description = "Public IP address of the app instance"
-  value       = module.app.instance_public_ip
-  sensitive   = true
-}
-
 output "app_lb_dns_name" {
   description = "Domain name of app load balancer"
   value       = module.end.app_lb_dns_name
