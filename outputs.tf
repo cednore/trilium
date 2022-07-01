50b25c35191839cca702b14fbf56713c72493816
cednore <cednore@gmail.com>
2022-07-01 07:06:24 -0700

    🔒️ sensitive outputs

--- outputs.tf
+++ outputs.tf
@@ -11,9 +11,11 @@ output "app_urls" {
 output "app_instance_public_ip" {
   description = "Public IP address of the app instance"
   value       = module.app.instance_public_ip
+  sensitive   = true
 }
 
 output "app_lb_dns_name" {
   description = "Domain name of app load balancer"
   value       = module.end.app_lb_dns_name
+  sensitive   = true
 }
