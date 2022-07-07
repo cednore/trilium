ecb494e9be6bd6f673431dcb7fbee8bed5ce924c
cednore <cednore@gmail.com>
2022-07-07 10:48:29 -0700

    ⚡ better initial delay at provision module

--- modules/trilium-provision/playbooks.tf
+++ modules/trilium-provision/playbooks.tf
@@ -1,4 +1,12 @@
+resource "time_sleep" "initial_delay" {
+  create_duration = "10s"
+}
+
 resource "null_resource" "app_data_volume_provisioner" {
+  depends_on = [
+    time_sleep.initial_delay,
+  ]
+
   triggers = {
     src_hash = filesha256("${path.module}/playbooks/app-data.yml")
     variables = jsonencode([
@@ -12,14 +20,14 @@ resource "null_resource" "app_data_volume_provisioner" {
 
   provisioner "local-exec" {
     command = <<BASH
-                sleep 10s && ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/playbooks/app-data.yml \
-                  -u ubuntu \
-                  -i '${var.app_instance_public_ip},' \
-                  --private-key ${var.app_keypair_path} \
-                  -e 'volume_serial=${replace(var.data_volume_id, "-", "")}' \
-                  -e 'mount_path=${var.data_volume_mount_path}' \
-                  -e 'filesystem=${var.data_volume_filesystem}'
-              BASH
+      ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/playbooks/app-data.yml \
+        -u ubuntu \
+        -i '${var.app_instance_public_ip},' \
+        --private-key ${var.app_keypair_path} \
+        -e 'volume_serial=${replace(var.data_volume_id, "-", "")}' \
+        -e 'mount_path=${var.data_volume_mount_path}' \
+        -e 'filesystem=${var.data_volume_filesystem}'
+    BASH
   }
 }
 
@@ -47,18 +55,18 @@ resource "null_resource" "app_instance_provisioner" {
 
   provisioner "local-exec" {
     command = <<BASH
-                sleep 10s && ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/playbooks/app.yml \
-                  -u ubuntu \
-                  -i '${var.app_instance_public_ip},' \
-                  --private-key ${var.app_keypair_path} \
-                  -e 'image=${var.app_image}' \
-                  -e 'tag=${var.app_image_tag}' \
-                  -e 'container_count=${var.app_container_count}' \
-                  -e 'container_name_prefix=${var.app_container_name_prefix}' \
-                  -e 'ports=${var.app_container_ports}' \
-                  -e 'volumes=${var.data_volume_mount_path}:${var.app_container_data_path}' \
-                  -e 'awslogs_region=${data.aws_region.current.name}' \
-                  -e 'awslogs_group=${var.app_container_log_group}'
-              BASH
+      ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ${path.module}/playbooks/app.yml \
+        -u ubuntu \
+        -i '${var.app_instance_public_ip},' \
+        --private-key ${var.app_keypair_path} \
+        -e 'image=${var.app_image}' \
+        -e 'tag=${var.app_image_tag}' \
+        -e 'container_count=${var.app_container_count}' \
+        -e 'container_name_prefix=${var.app_container_name_prefix}' \
+        -e 'ports=${var.app_container_ports}' \
+        -e 'volumes=${var.data_volume_mount_path}:${var.app_container_data_path}' \
+        -e 'awslogs_region=${data.aws_region.current.name}' \
+        -e 'awslogs_group=${var.app_container_log_group}'
+    BASH
   }
 }
