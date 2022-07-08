6f26c20f6a727302c887ce8b5d3d6df709143f8e
cednore <cednore@gmail.com>
2022-07-08 05:59:46 -0700

    💡 add comment

--- locals.tf
+++ locals.tf
@@ -1,5 +1,10 @@
 locals {
-  apex_domain = join(".", slice(split(".", var.domain), length(split(".", var.domain)) - 2, length(split(".", var.domain))))
+  apex_domain = join(".", slice(
+    split(".", var.domain),
+    length(split(".", var.domain)) - 2,
+    length(split(".", var.domain))
+    )
+  ) # Extract apex domain from app domain
 
   app_container_name_prefix = "app"
   data_volume_mount_path    = "/mnt/app_data"
