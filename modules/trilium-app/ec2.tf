709c4df33e3c17a450b6d6050f21a29ecb343194
cednore <cednore@gmail.com>
2022-07-18 14:00:01 -0700

    ⚡ better ni def

--- modules/trilium-app/ec2.tf
+++ modules/trilium-app/ec2.tf
@@ -1,10 +1,13 @@
 resource "aws_instance" "app" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
   key_name               = aws_key_pair.main.key_name
-  vpc_security_group_ids = var.sg_ids
   iam_instance_profile   = aws_iam_instance_profile.app.id
+
+  network_interface {
+    network_interface_id = aws_network_interface.app.id
+    device_index         = 0
+  }
 
   tags = merge(local.default_tags, {
     Name = "i-${local.slug}-app"
@@ -15,6 +18,15 @@ resource "aws_instance" "app" {
       ami, # Ignore ami updates
     ]
   }
+}
+
+resource "aws_network_interface" "app" {
+  subnet_id       = var.subnet_id
+  security_groups = var.sg_ids
+
+  tags = merge(local.default_tags, {
+    Name = "ni-${local.slug}-app"
+  })
 }
 
 resource "aws_key_pair" "main" {
