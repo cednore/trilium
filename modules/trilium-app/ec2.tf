7b587aef720c287ab7cac3dbe9ad81b0be8b878b
cednore <cednore@gmail.com>
2022-07-18 14:06:55 -0700

    🚨 just fmt

--- modules/trilium-app/ec2.tf
+++ modules/trilium-app/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "app" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.main.key_name
-  iam_instance_profile   = aws_iam_instance_profile.app.id
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = var.instance_type
+  key_name             = aws_key_pair.main.key_name
+  iam_instance_profile = aws_iam_instance_profile.app.id
 
   network_interface {
     network_interface_id = aws_network_interface.app.id
