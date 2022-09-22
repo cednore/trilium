2c346d6ac8279786c05f13f8b93d977027aab418
cednore <cednore@gmail.com>
2022-09-22 09:38:24 -0700

    🚚 move variables

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+variable "app_name" {
+  description = "Name of this app"
+  type        = string
+  default     = "Trilium Notes"
+}
+
+variable "app" {
+  description = "URL friendly name of this app"
+  type        = string
+  default     = "trilium"
+}
+
+variable "stage" {
+  description = "Stage of deployment"
+  type        = string
+  default     = "production"
+}
+
+variable "domain" {
+  description = "Domain name to host this app"
+  type        = string
+  default     = "trilium.someone.me"
+}
+
 locals {
   app_container_name_prefix = "app"
   data_volume_mount_path    = "/mnt/app_data"
