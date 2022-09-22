439f3829f2c0d74fab44ee96c791328daac55a93
cednore <cednore@gmail.com>
2022-09-22 11:52:35 -0700

    🐛 fix trigger

--- modules/trilium-provision/playbooks.tf
+++ modules/trilium-provision/playbooks.tf
@@ -9,13 +9,13 @@ resource "null_resource" "trilium_data_volume_provisioner" {
 
   triggers = {
     src_hash = filesha256("${path.module}/playbooks/trilium-data.yml")
-    dependencies = sha256(jsonencode([
+    dependencies = jsonencode([
       var.app_instance_public_ip,
       var.app_keypair_path,
       var.data_volume_id,
       var.data_volume_mount_path,
       var.data_volume_filesystem,
-    ]))
+    ])
   }
 
   provisioner "local-exec" {
@@ -34,7 +34,7 @@ resource "null_resource" "trilium_installer" {
 
   triggers = {
     src_hash = filesha256("${path.module}/playbooks/trilium.yml")
-    dependencies = sha256(jsonencode([
+    dependencies = jsonencode([
       var.app_instance_public_ip,
       var.app_keypair_path,
       var.app_image,
@@ -46,7 +46,7 @@ resource "null_resource" "trilium_installer" {
       var.app_container_data_path,
       data.aws_region.current.name,
       var.app_container_log_group,
-    ]))
+    ])
   }
 
   provisioner "local-exec" {
