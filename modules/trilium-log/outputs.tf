157b9c2f48b2fc84e4e6921b0e67ebb2302ed4bb
cednore <cednore@gmail.com>
2022-10-03 10:48:10 -0700

    ✨ add outputs

--- modules/trilium-log/outputs.tf
+++ modules/trilium-log/outputs.tf
@@ -1,3 +1,8 @@
+output "log_group_region" {
+  description = "Region of the log groups"
+  value       = data.aws_region.current.name
+}
+
 output "app_log_group" {
   description = "CloudWatch log group for app"
   value       = aws_cloudwatch_log_group.app.name
